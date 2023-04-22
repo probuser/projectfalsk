8d36759a9c39a8ec1d70b753daed777de535f6f4
admin <gautam.mktg556@gmail.com>
2023-04-22 17:22:06 +0530

    commented databse connection as it is not working[databse is removed]

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask import Flask, render_template
 import mysql.connector
 from mysql.connector import Error
 from sqlalchemy import text
+import requests
+import json
 
 
 app = Flask(__name__, template_folder='Template', static_folder="static")
@@ -19,6 +21,9 @@ app = Flask(__name__, template_folder='Template', static_folder="static")
 # cursor.execute("select * from database_work.travel_location;")
 # # get all records
 # records = cursor.fetchall()
+
+response = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=8ddbc01df6caa3c70f7bbc06d87e1331")
+print(response.text)
 
 @app.route('/')
 def home_page():
